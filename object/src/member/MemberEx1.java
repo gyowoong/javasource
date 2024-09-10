74c8936667bef012d55d96937452f4ce476f64a7
이교웅 <landjjang1212@gmail.com>
2024-09-10 15:04:47 +0900

    멤버 추가

--- object/src/member/MemberEx1.java
+++ object/src/member/MemberEx1.java
@@ -7,8 +7,17 @@ public class MemberEx1 {
     public static void main(String[] args) {
         List<Member> list = new ArrayList<>();
 
+        // list에 Member 추가
+        list.add(new Member("hong123", "홍길동", "서울", "hong@naver.com"))
+        list.add(new Member("hong124", "홍길동", "서울", "hong@naver.com"))
+        list.add(new Member("hong125", "홍길동", "서울", "hong@naver.com"))
+        list.add(new Member("hong126", "홍길동", "서울", "hong@naver.com"))
+
         for (Member member : list) {
-            System.out.println(member.getId());
+            System.out.println(member.getId() + "\t");
+            System.out.println(member.getName() + "\t");
+            System.out.println(member.getAddr() + "\t");
+            System.out.println(member.getEmail());
         }
     }
 
